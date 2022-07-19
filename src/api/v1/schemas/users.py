ae871b9f38f48f8a8db7aeb37e771e5b6be68a61
Alexander Lyotov <sippie84@gmail.com>
2022-07-19 16:59:16 +0300

    feature/UserCreate scheme

--- src/api/v1/schemas/users.py
+++ src/api/v1/schemas/users.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from typing import Optional, List
 from pydantic import BaseModel
 
 __all__ = (
@@ -15,13 +16,32 @@ class UserBase(BaseModel):
 
 
 class UserCreate(UserBase):
-    ...
+    username: str
+    email: str
+    password: str
 
 
 class UserModel(UserBase):
-    id: int
+    id: Optional[int]
+    username: str
+    roles: List[str]
     created_at: datetime
+    uuid:  str
+    is_totp_enabled: bool
+    is_active: bool
+    email: str
 
 
 class UserListResponse(BaseModel):
     users: list[UserModel] = []
+
+
+# class User(SQLModel, table=True):
+#     id: Optional[int] = Field(default=None, primary_key=True)
+#     username: str = Field(nullable=False)
+#     roles: List[str] = Field(default=[])
+#     created_at: datetime = Field(default=datetime.utcnow(), nullable=False)
+#     uuid:  str = Field()
+#     is_totp_enabled: bool = Field(default=False)
+#     is_active: bool = Field(default=True)
+#     email: str = Field(nullable=False)
