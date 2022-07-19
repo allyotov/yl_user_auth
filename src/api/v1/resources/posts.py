52de3a40ea4dbc8dc9ca3b11ebf3882e45ea3e7b
Alexander Lyotov <sippie84@gmail.com>
2022-07-19 15:31:21 +0300

    feature/line headings

--- src/api/v1/resources/posts.py
+++ src/api/v1/resources/posts.py
@@ -9,15 +9,8 @@ from src.services import PostService, get_post_service
 router = APIRouter()
 
 
-@router.get(
-    path="/",
-    response_model=PostListResponse,
-    summary="Список постов",
-    tags=["posts"],
-)
-def post_list(
-    post_service: PostService = Depends(get_post_service),
-) -> PostListResponse:
+@router.get(path="/", response_model=PostListResponse, summary="Список постов", tags=["posts"],)
+def post_list(post_service: PostService = Depends(get_post_service),) -> PostListResponse:
     posts: dict = post_service.get_post_list()
     if not posts:
         # Если посты не найдены, отдаём 404 статус
@@ -25,15 +18,8 @@ def post_list(
     return PostListResponse(**posts)
 
 
-@router.get(
-    path="/{post_id}",
-    response_model=PostModel,
-    summary="Получить определенный пост",
-    tags=["posts"],
-)
-def post_detail(
-    post_id: int, post_service: PostService = Depends(get_post_service),
-) -> PostModel:
+@router.get(path="/{post_id}",  response_model=PostModel, summary="Получить определенный пост", tags=["posts"],)
+def post_detail(post_id: int, post_service: PostService = Depends(get_post_service),) -> PostModel:
     post: Optional[dict] = post_service.get_post_detail(item_id=post_id)
     if not post:
         # Если пост не найден, отдаём 404 статус
@@ -41,14 +27,7 @@ def post_detail(
     return PostModel(**post)
 
 
-@router.post(
-    path="/",
-    response_model=PostModel,
-    summary="Создать пост",
-    tags=["posts"],
-)
-def post_create(
-    post: PostCreate, post_service: PostService = Depends(get_post_service),
-) -> PostModel:
+@router.post(path="/", response_model=PostModel, summary="Создать пост", tags=["posts"],)
+def post_create(post: PostCreate, post_service: PostService = Depends(get_post_service),) -> PostModel:
     post: dict = post_service.create_post(post=post)
     return PostModel(**post)
