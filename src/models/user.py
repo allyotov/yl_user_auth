442d67824c4ff4386283ca10c9121b23bda691d2
Alexander Lyotov <sippie84@gmail.com>
2022-07-20 10:19:14 +0300

    feature/roles and password fields

--- src/models/user.py
+++ src/models/user.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from email.policy import default
 from typing import Optional, List
+from sqlalchemy import null
 
 from sqlmodel import Field, SQLModel
 
@@ -10,9 +11,10 @@ __all__ = ("User",)
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(nullable=False)
-    roles: List[str] = Field(default=[])
+    roles: List[str] = Field(default=['common_user'])
     created_at: datetime = Field(default=datetime.utcnow(), nullable=False)
     uuid:  str = Field()
     is_totp_enabled: bool = Field(default=False)
     is_active: bool = Field(default=True)
     email: str = Field(nullable=False)
+    password: str = Field(nullable=False)
