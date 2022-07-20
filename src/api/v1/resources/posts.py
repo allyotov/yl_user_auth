142be242e0363cc7737d253b2016375344882200
Alexander Lyotov <sippie84@gmail.com>
2022-07-20 21:44:29 +0300

    feature/check login in create post

--- src/api/v1/resources/posts.py
+++ src/api/v1/resources/posts.py
@@ -1,13 +1,15 @@
 from http import HTTPStatus
 from typing import Optional
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Security
+from fastapi.security import OAuth2PasswordBearer, HTTPBearer, HTTPAuthorizationCredentials
 
 from src.api.v1.schemas import PostCreate, PostListResponse, PostModel
-from src.services import PostService, get_post_service
+from src.models import user
+from src.services import PostService, UserService, get_post_service, get_user_service
 
 router = APIRouter()
-
+security = HTTPBearer()
 
 @router.get(path="/", response_model=PostListResponse, summary="Список постов", tags=["posts"],)
 def post_list(post_service: PostService = Depends(get_post_service),) -> PostListResponse:
@@ -28,6 +30,12 @@ def post_detail(post_id: int, post_service: PostService = Depends(get_post_servi
 
 
 @router.post(path="/", response_model=PostModel, summary="Создать пост", tags=["posts"],)
-def post_create(post: PostCreate, post_service: PostService = Depends(get_post_service),) -> PostModel:
+def post_create(post: PostCreate, 
+        credentials: HTTPAuthorizationCredentials = Security(security),
+        post_service: PostService = Depends(get_post_service), 
+        user_service: UserService = Depends(get_user_service),) -> PostModel:
+    access_token = credentials.credentials
+    user_service.check_token_if_blocked(access_token)
+    user_service.get_current_user(access_token)
     post: dict = post_service.create_post(post=post)
     return PostModel(**post)
