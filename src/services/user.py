f092ff1a66116f8b05157ddc6cf94268ecf9f247
Alexander Lyotov <sippie84@gmail.com>
2022-07-20 21:46:34 +0300

    chore/removed commented strings

--- src/services/user.py
+++ src/services/user.py
@@ -117,29 +117,6 @@ class UserService(ServiceMixin):
         except Exception as e:
             raise HTTPException(status_code=500, detail='Can\'t edit user in database. {}'.format(e))
 
-    # def get_user_list(self) -> dict:
-    #     """Получить список пользователей."""
-    #     users = self.session.query(User).order_by(User.created_at).all()
-    #     return {"user": [UserModel(**user.dict()) for user in users]}
-
-    # def get_user_detail(self, item_id: int) -> Optional[dict]:
-    #     """Получить детальную информацию пользователя."""
-    #     if cached_user := self.cache.get(key=f"user{item_id}"):
-    #         return json.loads(cached_user)
-    #     user = self.session.query(User).filter(User.id == item_id).first()
-    #     if user:
-    #         self.cache.set(key=f"user{user.id}", value=user.json())
-    #     return user.dict() if user else None
-
-    # def create_user(self, user: UserCreate) -> dict:
-    #     """Создать пользователя."""
-
-    #     # check if user is already in db
-    #     new_user = User(title=user.username, description=user.email, password=user.password)
-    #     self.session.add(new_user)
-    #     self.session.commit()
-    #     self.session.refresh(new_user)
-    #     return new_user.dict()
 
 
 # get_user_service — это провайдер UserService. Синглтон
