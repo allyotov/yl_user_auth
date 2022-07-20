a4ae34c12c4aefed8f520cce38df1df964fdddc1
Alexander Lyotov <sippie84@gmail.com>
2022-07-20 10:17:41 +0300

    feature/signup endpoint

--- src/api/v1/resources/users.py
+++ src/api/v1/resources/users.py
@@ -1,22 +1,26 @@
+import logging
+
 from http import HTTPStatus
 from typing import Optional
 
 from fastapi import APIRouter, Depends, HTTPException
 
-from src.api.v1.schemas import UserCreate, UserListResponse, UserModel
-from src.services import AuthService, get_auth_service
+from src.api.v1.schemas import UserCreate, UserListResponse, UserModel, UserCreated
+from src.services import UserService, get_user_service
 
 router = APIRouter()
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 @router.post(
     path="/signup",
-    response_model=UserModel,
+    response_model=UserCreated,
     summary="Зарегистрироваться",
     tags=["auth"],
 )
-def signup(
-    user: UserCreate, auth_service: AuthService = Depends(get_auth_service),
-) -> UserModel:
-    user: dict = auth_service.signup(user=user)
-    return UserModel(**user)
+def signup(user: UserCreate, user_service: UserService = Depends(get_user_service),) -> UserCreated:
+    logger.debug(user)
+    user = user_service.signup(user_details=user)
+    logger.debug(user)
+    return UserCreated(username=user['username'], email=user['email'])
