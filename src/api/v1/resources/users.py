4eb37ecaba565b6b46bd7cca4cbb95c2b410ebf9
Alexander Lyotov <sippie84@gmail.com>
2022-07-20 15:02:06 +0300

    feature/users/me endpoint

--- src/api/v1/resources/users.py
+++ src/api/v1/resources/users.py
@@ -4,6 +4,7 @@ from http import HTTPStatus
 from typing import Optional
 
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 
 from src.api.v1.schemas import UserCreate, UserListResponse, UserModel, UserCreated, UserAuth, Tokens
 from src.services import UserService, get_user_service
@@ -13,8 +14,10 @@ router = APIRouter()
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+reuseable_oauth = OAuth2PasswordBearer(tokenUrl='/login', scheme_name='JWT')
 
-@router.post(path="/signup", response_model=UserCreated, summary="Зарегистрироваться", tags=["auth"],)
+
+@router.post(path="/signup", response_model=UserCreated, summary="Зарегистрироваться", tags=["auth"], status_code=201)
 def signup(user: UserCreate, user_service: UserService = Depends(get_user_service),) -> UserCreated:
     logger.debug(user)
     user = user_service.signup(user_details=user)
@@ -28,3 +31,10 @@ def login(user: UserAuth, user_service: UserService = Depends(get_user_service),
     tokens = user_service.login(user_details=user)
     logger.debug(tokens)
     return Tokens(**tokens)
+
+
+@router.get(path="/users/me", response_model=UserModel, summary="Посмотреть информацию о себе", tags=["auth"],)
+def get_me(token: str = Depends(reuseable_oauth), user_service: UserService = Depends(get_user_service),) -> UserModel:
+    logger.debug('token')
+    logger.debug(token)
+    return user_service.get_current_user(token)
