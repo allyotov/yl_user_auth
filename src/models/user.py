22786e58963acc1fb168c69f34cdca3d51699227
Alexander Lyotov <sippie84@gmail.com>
2022-07-20 15:03:11 +0300

    feature/roles MutableList type field

--- src/models/user.py
+++ src/models/user.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from email.policy import default
 from typing import Optional, List
 from sqlalchemy import null
+from sqlalchemy.ext.mutable import MutableList
 
 from sqlmodel import Field, SQLModel
 
@@ -11,7 +12,7 @@ __all__ = ("User",)
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(nullable=False)
-    roles: List[str] = Field(default=['common_user'])
+    roles: MutableList[str] = Field(default=['common_user'])
     created_at: datetime = Field(default=datetime.utcnow(), nullable=False)
     uuid:  str = Field()
     is_totp_enabled: bool = Field(default=False)
